3745cd776e8131a7065ae7209813a4bf9710e6f0
Rahul <kasurirahul6@gmail.com>
2024-12-19 14:52:51 +0530

    first-commit

---
+++ main.py
@@ -0,0 +1,146 @@
+# #control structures in py
+# # 1.conditional statements
+# # 2.looping statements
+# # 3.control statements
+# #1.simple if
+# # a=int(input())
+# # if(a==5):
+# #     print("equal")
+# # else:
+# #     print("not equal")
+# # print("equal" if  a== 5 else "not equal")
+# # print("equal" if int(input()) == 5 else "not equal")
+# # a=int(input())
+# # if(a&1==0):
+# #     print("eeven")
+# # else:
+# #     print("odd")
+# # a=int(input())
+# # if((a//2)*2==a):
+# #     print("even")
+# # else:
+# #     print("odd")
+# # a=int(input())
+# # b=int(input())
+# # c=int(input())
+# # if(a>b and a>c):
+# #     print(a,"is greater")
+# # elif(b>c):
+# #     print(b, "iss greater")
+# # else:
+# #     print(c ,"is grater")
+a=20
+b=30
+c=a+b
+a=c-a
+b=c-b
+print(a)
+print(b)
+# if(condition-1):
+#     statement-block 1
+# else:
+#     statement
+
+
+a = int(input())
+b = str(a)
+print(len(b))
+# <p class="card-subtitle">User-Authentication</p>
+
+#                   <h3 class="h3 card-title">This application securely stores user data in MongoDB and includes verification mechanisms to ensure authenticity.
+#                      I implemented Nodemailer for sending verification and notification emails,
+#                      enhancing user engagement and security.</h3>
+#                      <span>
+#                       <p class="card-subtitle">Technology Stack</p>
+#                       <p>Node.js, JavaScript XML, MongoDB, Tailwind CSS</p>
+#                      </span>
+#                   <span class="btn-link">
+#                     <span>View Project</span>
+
+#                     <ion-icon name="arrow-forward"></ion-icon>
+#                   </span>
+
+
+# <p class="card-subtitle">Hepatitis C Disease Prediction</p>
+#                   <h3 class="h3 card-title">
+#                     Developed a Machine Learning Model to predict Hepatitis C using patient data, achieving an impressive 91% accuracy.
+#                     The project involved preprocessing and analyzing medical datasets to identify key features critical for prediction.
+#                   </h3>
+#                   <span>
+#                     <p class="card-subtitle">Technology Stack</p>
+#                     <p>Python, k-NN, Random Forest, Linear Regression</p>
+#                   </span>
+#                   <span class="btn-link">
+#                     <span>View Project</span>
+#                     <ion-icon name="arrow-forward"></ion-icon>
+#                   </span>
+
+
+
+# <p class="card-subtitle">Lane detection</p>
+
+#                   <h3 class="h3 card-title">Developed a robust lane detection system using Python and OpenCV, aimed at enhancing vehicle safety by accurately identifying road lanes in video input
+#                     mplemented Canny Edge Detection to extract prominent edges by reducing noise through Gaussian blurring and detecting intensity gradients, ensuring precision in identifying lane boundaries.
+#                   </h3>
+#                     <span>
+#                       <p class="card-subtitle">Technology Stack</p>
+#                       <p>Python, OpenCV, Canny Edge Detection, HLT</p>
+#                     </span>
+#                   <span class="btn-link">
+#                     <span>View Project</span>
+
+#                     <ion-icon name="arrow-forward"></ion-icon>
+#                   </span>
+
+
+
+# p class="card-subtitle">Doctor Appointment Booking.</p>
+
+#                   <h3 class="h3 card-title">This System is built with Express, JavaScript, and Tailwind CSS. The application allows users to book and manage appointments,
+#                      with an integrated online payment method for secure transactions. I utilized Express for the backend to handle scheduling, user authentication, and payment processing</h3>
+                  
+#                     <span>
+#                       <p class="card-subtitle">Technology Stack</p>
+#                   <p>JavaScript, JavaScript XML, express, Node.js, Tailwind CSS, MongoDB</p>
+
+#                     </span>
+#                      <span class="btn-link">
+                    
+#                     <span>View Project</span>
+                  
+
+
+
+#                   <p class="card-subtitle">Weather Application</p>
+
+#                   <h3 class="h3 card-title">Developed a Weather Application using the OpenWeatherAPI,and this application allows users to check real-time weather by entering the name of any city.</h3>
+#                   <span>
+#                     <p class="card-subtitle">Technology Stack</p>
+#                     <p>HTML, CSS, OpenWeatherAPI</p>
+#                   </span>
+#                   <span class="btn-link">
+                    
+#                   <span>View Project</span>
+
+#                   <ion-icon name="arrow-forward"></ion-icon>
+#                   </span>
+
+
+
+
+
+
+# <p class="card-subtitle">Lane detection</p>
+
+#                   <h3 class="h3 card-title">Developed a robust lane detection system using Python and OpenCV, aimed at enhancing vehicle safety by accurately identifying road lanes in video input
+#                     mplemented Canny Edge Detection to extract prominent edges by reducing noise through Gaussian blurring and detecting intensity gradients, ensuring precision in identifying lane boundaries.
+#                   </h3>
+#                     <span>
+#                       <p class="card-subtitle">Technology Stack</p>
+#                       <p>Python, OpenCV, Canny Edge Detection, HLT</p>
+#                     </span>
+#                   <span class="btn-link">
+#                     <span>View Project</span>
+
+#                     <ion-icon name="arrow-forward"></ion-icon>
+#                   </span>
